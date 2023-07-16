8455bdafe2ce891893930a7b32b473899ac5c67e
joaovitor27 <joaovitor.monteiro.188@gmail.com>
2023-07-16 18:38:42 -0300

    implementação do front

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DjangoAlura.apps.DjangoaluraConfig',
+    'gallery.apps.GalleryConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'djangoProject/static'
+]
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- gallery/apps.py
+++ gallery/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjangoaluraConfig(AppConfig):
+class GalleryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'DjangoAlura'
+    name = 'gallery'
