21f2ded506806dca19d8392eb49f12c512b7ab73
joaovitor27 <joaovitor.monteiro.188@gmail.com>
2023-07-17 09:56:07 -0300

    conclinto integração com o banco de dados

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gallery.apps.GalleryConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,6 +129,11 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- gallery/views.py
+++ gallery/views.py
@@ -13,3 +13,14 @@ def index(request):
 def image(request, id_: int):
     photo: QuerySet | Http404 = get_object_or_404(Photography, id=id_)
     return render(request, "gallery/image.html", {'photo': photo})
+
+
+def search(request):
+    search_ = request.GET.get('search')
+    if search is None or not search:
+        photography: QuerySet = Photography.objects.order_by('created_at').filter(published=True)
+    else:
+        photography: QuerySet = Photography.objects.order_by('created_at').filter(
+            published=True, name__icontains=search_
+        )
+    return render(request, "gallery/search.html", {'photography': photography})
